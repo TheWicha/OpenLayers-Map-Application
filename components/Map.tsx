dd9f2ea7beda2cd9d5ed39d12ca8a372ec0baa6c
Daniel Wisniewski <wicha@daniels-mbp.home>
2024-06-13 14:06:11 +0200

    move ol map to its own hook

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,39 +1,10 @@
 "use client";
-import React, { useEffect, useRef } from "react";
-import Map from "ol/Map";
-import View from "ol/View";
-import TileLayer from "ol/layer/Tile";
-import OSM from "ol/source/OSM";
-import { useGeographic } from "ol/proj";
+import React, { useRef } from "react";
+import useMap from "@/hooks/useMap";
 
 const MapComponent = () => {
   const mapRef = useRef(null);
-  const initialMapRef = useRef<Map | null>(null);
-
-  useGeographic();
-
-  useEffect(() => {
-    if (mapRef.current) {
-      initialMapRef.current = new Map({
-        target: mapRef.current,
-        layers: [
-          new TileLayer({
-            source: new OSM(),
-          }),
-        ],
-        view: new View({
-          center: [19.1451, 51.9194],
-          zoom: 7,
-        }),
-      });
-      initialMapRef.current.updateSize();
-    }
-    return () => {
-      if (mapRef.current && initialMapRef.current) {
-        initialMapRef.current.setTarget(undefined);
-      }
-    };
-  }, []);
+  useMap(mapRef);
 
   return <div ref={mapRef} className="w-full h-screen" />;
 };
---
+++ hooks/useMap.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import { useGeographic } from "ol/proj";
+
+const useMap = (mapRef: React.RefObject<HTMLDivElement>) => {
+  const initialMapRef = useRef<Map | null>(null);
+
+  useGeographic();
+
+  useEffect(() => {
+    if (mapRef.current) {
+      initialMapRef.current = new Map({
+        target: mapRef.current,
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+        ],
+        view: new View({
+          center: [19.1451, 51.9194],
+          zoom: 7,
+        }),
+      });
+      initialMapRef.current.updateSize();
+    }
+    return () => {
+      if (mapRef.current && initialMapRef.current) {
+        initialMapRef.current.setTarget(undefined);
+      }
+    };
+  }, [mapRef]);
+
+  return initialMapRef.current;
+};
+
+export default useMap;
