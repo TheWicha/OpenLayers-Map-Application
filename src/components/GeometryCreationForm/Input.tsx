f7cbd762d5f9e3389add572d7cb3c9020e89eaa2
Daniel Wisniewski <wicha@daniels-mbp.home>
2024-06-14 07:55:36 +0200

    update input props

--- src/components/GeometryCreationForm/Input.tsx
+++ src/components/GeometryCreationForm/Input.tsx
@@ -3,7 +3,7 @@ import React from "react";
 interface InputProps {
   setVal: (name: string, val: string) => void;
   value: string | string[];
-  type: "text" | "date" | "textbox";
+  type: "text" | "date" | "textarea";
   disabled?: boolean;
   name: string;
   title: string;
@@ -12,7 +12,7 @@ interface InputProps {
 const Input = ({ setVal, value, type, name, disabled, title }: InputProps) => {
   const today = new Date().toISOString().split("T")[0];
 
-  if (type === "textbox") {
+  if (type === "textarea") {
     return (
       <label className="flex flex-col items-center">
         {title}:
